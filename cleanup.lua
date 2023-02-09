4f86ea3fea132c983a78de279811014d0a0a1971
Quad <82482373+Quadddd@users.noreply.github.com>
2023-02-09 14:44:14 -0500

    Update cleanup.lua

--- cleanup.lua
+++ cleanup.lua
@@ -1,20 +1,26 @@
-shared._cleanup_event_ = shared._cleanup_event_ or Instance.new("BindableEvent")
-shared._cleanup_event_:Fire()
+shared.CLEANUP_EVENT = shared.CLEANUP_EVENT or Instance.new("BindableEvent")
+shared.CLEANUP_EVENT:Fire()
 
 local cleanup = {}
 
 cleanup._connections = {}
+cleanup._outdated = false
 
 function cleanup:WatchConnection(conn)
     assert(conn, "No connection passed")
     assert(typeof(conn) == 'RBXScriptConnection', ('Connection must be of type RBXScriptConnection. Got type: %s'):format(typeof(conn)))
 
-    table.insert(cleanup._connections, conn)
+    table.insert(self._connections, conn)
+end
+
+function cleanup:IsOutdated()
+    return self._outdated
 end
 
 task.spawn(function()
-    shared._cleanup_event_.Event:Wait()
+    shared.CLEANUP_EVENT.Event:Wait()
     
+    shared._outdated = true
     for _, conn in pairs(cleanup._connections) do
         if typeof(conn) ~= 'RBXScriptConnection' then continue end
         conn:Disconnect()
